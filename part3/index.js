15addfd4150ad9f726f2effcbc2b6208737a7305
Amurensy <77088694+IsaacSoriaSC@users.noreply.github.com>
2023-12-28 16:11:01 -0500

    Completed Exercise 3.7

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let address = [
   { 
@@ -83,6 +85,12 @@ app.get('/', (request, response) => {
     
       response.json(newAddr)
     })
+
+    const unknownEndpoint = (request, response) => {
+      response.status(404).send({ error: 'unknown endpoint' })
+    }
+    
+    app.use(unknownEndpoint)
 
   const PORT = 3001
   app.listen(PORT, () => {
