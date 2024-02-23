91c2c8002fcd33af5db4531ae9060ff6769d77b6
unknown <77088694+IsaacSoriaSC@users.noreply.github.com>
2024-02-23 12:51:34 -0500

    Completed Exercise 3.13

--- part3/index.js
+++ part3/index.js
@@ -1,52 +1,40 @@
+require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const Entrie = require('./models/phone')
+const mongoose = require('mongoose')
+
+
 
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
-app.use(morgan('tiny'))
 
 
-let address = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+/*let address = Entrie.find({}).then(result => {
+  const entries = result.map(entrie => {
+    return {
+      name: entrie.name,
+      number: entrie.number
+    };
+  });
+  return entries;
+});*/
+ 
+app.get('/api/persons', (request, response) => {
+  Entrie.find({})
+    .then(entries => {
+      console.log(entries)
+      response.json(entries);
+    })
+    .catch(error => {
+      console.error('Error fetching entries:', error);
+      response.status(500).json({ error: 'Internal server error' });
+    });
+});
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-  })
-  
-  app.get('/api/persons', (request, response) => {
-    response.json(address)
-  })
-
-  app.get('/info', (request, response) => {
-    const respuesta = `<p>Phonebook has info for ${address.length} people </p>`
-    const fecha = new Date()
-    response.send(respuesta + fecha)
-  })
-
-  app.get('/api/persons/:id', (request, response) => {
+    app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const addre = address.find(addr => addr.id === id)
     if (addre) {
@@ -55,51 +43,28 @@ app.get('/', (request, response) => {
       response.status(404).end()
     }  })
   
-    app.delete('/api/persons/:id', (request, response) => {
-      const id = Number(request.params.id)
-      address = address.filter(addr => addr.id !== id)
-    
-      response.status(204).end()
+    app.get('/api/persons/:id', (request, response) => {
+      Note.findById(request.params.id).then(note => {
+        response.json(note)
+      })
     })
-
-    const generateId = () => {
-      return Math.floor(Math.random() * 1000000)
-    }
     
-    morgan.token('post', function (req, res) { return JSON.stringify(req.body) })
-
-    app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post', {
-      skip: function (req, res) { return req.method != 'POST' }
-    }))
-
-    app.use(morgan('tiny', {
-      skip: function (req, res) { return req.method === 'POST' }
-    }))
 
     app.post('/api/persons', (request, response) => {
       const body = request.body
     
-      if ((!body.name || !body.number)){
-        return response.status(400).json({ 
-          error: 'content missing' 
-        })
-      } if (address.find(addr => addr.name === body.name)){
-        return response.status(400).json({ 
-          error: 'name must be unique' 
-        })
+      if (body.name === undefined) {
+        return response.status(400).json({ error: 'content missing' })
       }
-      
-      
-
-      const newAddr = {
-        id: generateId(),
+    
+      const entrie = new Entrie({
         name: body.name,
         number: body.number,
-      }
+      })
     
-      address = address.concat(newAddr)
-    
-      response.json(newAddr)
+      entrie.save().then(savedEntrie => {
+        response.json(savedEntrie)
+      })
     })
 
     const unknownEndpoint = (request, response) => {
@@ -108,7 +73,7 @@ app.get('/', (request, response) => {
     
     app.use(unknownEndpoint)
 
-  const PORT = process.env.PORT || 3001
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`)
-    })
+    const PORT = process.env.PORT
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+      })
